6e7fbe1e92004b174549bdaf9440a9f50b9695e5
Ahmedomar02 <ahmedalimrany@gmail.com>
2025-03-03 15:17:02 +0200

    caluclator website add

---
+++ follower cou proj/followers_count.js
@@ -0,0 +1,14 @@
+var count = 0;
+function increaseCount() {
+
+    count++;
+    document.getElementById("countDisplay").innerText = count;
+    checkCountValue();
+}
+function checkCountValue() {
+    if (count === 10) {
+        alert("Your Instagram post gained 10 followers! Congratulations!");
+    } else if (count === 20) {
+        alert("Your Instagram post gained 20 followers! Keep it up!");
+    }
+}
